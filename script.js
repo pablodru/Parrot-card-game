7c2b625f8df01518266ce0fbeebf05e4cfb6c9f4
pablodru <pdrocha03@gmail.com>
2023-04-06 14:58:38 -0300

    versao com cartas virando no clique

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ while (numberCards % 2 !== 0 || numberCards > 14 || numberCards < 4) {
   );
 }
 
+onclick="cardChosen()"
+
 const arrayCards = [
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -14,7 +16,7 @@ const arrayCards = [
       <img class="image" src="img/bobrossparrot.gif" alt="bobross" />
     </div>
   </div>`,
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -22,7 +24,7 @@ const arrayCards = [
       <img class="image" src="img/explodyparrot.gif" alt="bobross" />
     </div>
   </div>`,
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -30,7 +32,7 @@ const arrayCards = [
       <img class="image" src="img/fiestaparrot.gif" alt="bobross" />
     </div>
   </div>`,
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -38,7 +40,7 @@ const arrayCards = [
       <img class="image" src="img/metalparrot.gif" alt="bobross" />
     </div>
   </div>`,
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -46,7 +48,7 @@ const arrayCards = [
       <img class="image" src="img/revertitparrot.gif" alt="bobross" />
     </div>
   </div>`,
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -54,7 +56,7 @@ const arrayCards = [
       <img class="image" src="img/tripletsparrot.gif" alt="bobross" />
     </div>
   </div>`,
-  `<div class="card">
+  `<div class="card" onclick="cardChosen(this)">
     <div class="front-face face">
       <img class="image" src="img/back.png" alt="card" />
     </div>
@@ -66,7 +68,7 @@ const arrayCards = [
 
 const listCards = [];
 const listPairCards = [];
-for (let i = 0; i < numberCards; i++) {
+for (let i = 0; i < numberCards/2; i++) {
   listCards.push(arrayCards[i]);
   listPairCards.push(arrayCards[i]);
 }
@@ -77,11 +79,38 @@ function comparator() {
 
 const number = comparator
 
-listCards.sort(number);
-listPairCards.sort(number);
+listCards.sort(comparator);
+listPairCards.sort(comparator);
+
+let listFinal = [];
+for (let j=0; j<listCards.length; j++) {
+    listFinal.push(listCards[j]);
+    listFinal.push(listCards[j]);
+}
 
 const main = document.querySelector(".main");
 
-for (let i = 0; i < numberCards / 2; i++) {
-  main.innerHTML += listCards[i] + listPairCards[i];
+listFinal = listFinal.sort(comparator);
+
+for (let i = 0; i < numberCards; i++) {
+  main.innerHTML += listFinal[i]
+}
+
+let selectedCards =[];
+
+function cardChosen(chosen) {
+    console.log(chosen)
+
+    const frente = chosen.firstElementChild;
+    frente.classList.add('selected-front')
+    const verso = chosen.lastElementChild;
+    verso.classList.add('selected-back')
+
+    let content = verso.querySelector('img')
+    selectedCards.push(content.src);
+
+    if (selectedCards.length%2==0 && selectedCards[-1]===selectedCards[-2]){
+        alert('boa')
+    }
+    
 }
